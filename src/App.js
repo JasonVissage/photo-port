00a247a6ee951f3a4ed551af82b25463d83d6f8a
Jason Vissage <92236402+JasonVissage@users.noreply.github.com>
2022-03-20 21:47:05 -0500

    add about page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Nav from './components/Nav';
+import About from './components/About';
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Nav></Nav>
+      <main>
+        <About></About>
+      </main>
+    </div>
+  );
 }
 
 export default App;
